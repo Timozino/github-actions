9d9541a5c7ea2d0dcf014a42135ce3ba75802ad0
Timozino <timsonfoli@gmail.com>
2023-11-24 21:45:11 +0100

    github actions tasks

--- remotescrapper/explain.py
+++ remotescrapper/explain.py
@@ -1,9 +1,36 @@
-ingryd_stud={'name':'Joan', 'course':'Data Sci'}
+import cv2
 
-lagos_state_govt={'scholarship':{'ingryd_stud':{'name':'Joan', 'course':'Data Sci'}}}
+def livestream():
+    # Open the default camera (usually the built-in webcam)
+    cap = cv2.VideoCapture(0)
 
-names=['Joan', 'Tim', 'Moshood']
+    # Check if the camera opened successfully
+    if not cap.isOpened():
+        print("Error: Could not open camera.")
+        return
 
-for name in names:
-    print(name[0])
+    # Set the window name
+    window_name = 'Live Stream'
 
+    while True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        # Check if the frame was captured successfully
+        if not ret:
+            print("Error: Failed to capture frame.")
+            break
+
+        # Display the frame in a window
+        cv2.imshow(window_name, frame)
+
+        # Break the loop if 'q' key is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release the camera and close the window
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    livestream()
