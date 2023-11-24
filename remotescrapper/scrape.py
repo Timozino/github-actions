a4c8190d9bc7de560abd568dd16bd295d07c7fe2
Folorunso Timson <110051396+Timozino@users.noreply.github.com>
2023-11-24 22:49:39 +0100

    Update scrape.py

--- remotescrapper/scrape.py
+++ remotescrapper/scrape.py
@@ -6,10 +6,15 @@ REQUEST_HEADER = {
     'user-agent': USER_AGENT,
     'Accept-Language': 'en-US, en;q=0.5',
 }
+
+
 def get_job_postings():
+
+               
     res = requests.get(url=BASE_URL, headers=REQUEST_HEADER)
     return res.json()
 def output_jobs_to_xls(data):
+    
     wb = Workbook()
     job_sheet = wb.add_sheet('Jobs')
     headers = list(data[0].keys())
@@ -17,6 +22,8 @@ def output_jobs_to_xls(data):
         job_sheet.write(0, i, header)
     for i, job in enumerate(data):
         values = list(job.values())
+
+        
         for x, value in enumerate(values):
             job_sheet.write(i + 1, x, value)
     wb.save('remote_job.xls')
